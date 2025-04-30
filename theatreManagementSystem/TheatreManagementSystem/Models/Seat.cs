94696d6e3c272510179d5f6272b3d004aefa5a39
“cedgoldi3@gmail.com [200~git config --global user.name “ivan <cedgoldi3@gmail.com>
2025-04-30 15:43:19 +0200

    Reinitialize Git and commit all project files

---
+++ theatreManagementSystem/TheatreManagementSystem/Models/Seat.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TheatreManagementSystem.Models
+{
+    public class Seat
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public long Id { get; set; }
+
+        [Required]
+        [ForeignKey("TheatreId")]
+        public Theatre Theatre { get; set; }
+        public long TheatreId { get; set; }
+
+        [Required]
+        public int ScreenNumber { get; set; }
+
+        [Required]
+        public string RowName { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        public int SeatNumber { get; set; }
+
+        [Required]
+        public SeatType SeatType { get; set; }
+
+        [Required]
+        [Range(0, double.MaxValue)]
+        public double PriceMultiplier { get; set; }
+    }
+
+    public enum SeatType
+    {
+        STANDARD,
+        PREMIUM,
+        VIP,
+        ACCESSIBLE
+    }
+}
